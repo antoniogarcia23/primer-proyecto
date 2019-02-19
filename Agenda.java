6f65d494a44f773ecca01f5b2e7eb84b39f2101e
Antonio Garcia <logost808@gmail.com>
2019-02-19 09:45:13 +0100

    cambio antonio 09.45 Change-Id: I091d66ed85cb7cd090bbc57b68fc74a39690206a Signed-off-by: Antonio Garcia <logost808@gmail.com>

--- Agenda.java
+++ Agenda.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Agenda
 {
 	/**
-	 * Atributos cambiados por antonio.
+	 * Atributos cambiados por antonio..
 	 */
 	private int numContactos;
 	private Contacto array[];	
